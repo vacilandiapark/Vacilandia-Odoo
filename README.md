9e5d19518899919be93ee9d362e77a7dd02db7c0
Vacilandia Park <115043853+vacilandiapark@users.noreply.github.com>
2024-06-01 12:34:17 -0500

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Vacilandia-Odoo" 
--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const swiperEl = document.getElementById('carousel_promotions')
-const swiperE2 = document.getElementById('carousel_announcements')
-const swiperE3 = document.getElementById('carousel_atractions')
+const swiperEl = document.getElementById('carrusel-banner')
+const swiperE2 = document.getElementById('carrusel-promotions')
 
 const params = {
     injectStyles: [`
       .swiper-button-next,
       .swiper-button-prev{
-        background: rgba(255,255,255);
-        padding: 2px 10px;
-        border-radius: 50%;
+        background: rgba(0,0,0, 0.9);
+        padding: 2px;
         z-index: 2;
-        box-shadow: 0 0 25px 3px rgba(0, 0, 0, .166);
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
       }
 
-      
+      .swiper-button-next:hover,
+      .swiper-button-prev:hover{
+        background: rgba(0,0,0,1);
+      }
+
       .swiper-button-next svg,
       .swiper-button-prev svg{
-        color: black;
-        width: 7px;
+        color: white;
+        width: 8px;
         text-align: center;
       }
       `],
@@ -26,8 +28,6 @@ const params = {
 
 Object.assign(swiperEl, params)
 Object.assign(swiperE2, params)
-Object.assign(swiperE3, params)
 
 swiperEl.initialize();
 swiperE2.initialize();
-swiperE3.initialize();
